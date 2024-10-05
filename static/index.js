7a0ef7fa89d518f2c8190030a3914c5943466b31
Mewtwocoder <damienweston915@gmail.com>
2024-10-05 18:04:03 -0400

    Changed height of iframe

--- static/index.js
+++ static/index.js
@@ -649,7 +649,8 @@ const runService = async (url, override, overrideadrbar) => {
         }
       }
       iframe.className = "extFrame";
-      iframe.style.width = "110%";
+      iframe.style.width = "100%";
+      iframe.style.height = "90%";
       iframe.style.border = "none";
       iframe.style.background = "#111";
       tabElement.appendChild(iframe);
